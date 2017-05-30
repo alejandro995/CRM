4465d884c93adaf173248e965197a55e96274b5a
alejandro995 <alejodt995@gmail.com>
2017-05-30 09:04:34 -0500

    Commits
    cambios front-end

--- eecc-pom/ecce-war/src/main/java/co/com/primeFaces/DataTableView.java
+++ eecc-pom/ecce-war/src/main/java/co/com/primeFaces/DataTableView.java
@@ -6,17 +6,22 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import org.primefaces.model.chart.PieChartModel;
+
+import org.primefaces.model.chart.*;
 
 @ManagedBean(name = "dataTableView")
 @ViewScoped
 public class DataTableView implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private LineChartModel lineModel1;
     private PieChartModel pieModel1;
+    private PieChartModel pieModel2;
     private  List<Programacion> programacionList = new ArrayList<Programacion>();
 
     private List<Preaprobados> preaprobadosList = new ArrayList<Preaprobados>();
+
+
 
     @PostConstruct
     public void init() {
@@ -69,7 +74,13 @@ public class DataTableView implements Serializable {
         preaprobadosList.add(new Preaprobados("Onboarding5","Sin asignar 20","Planificado 10"));
         preaprobadosList.add(new Preaprobados("Onboarding5","Asignados 40","Gestionada 0"));
 
+        createLineModels();
+
+
+
     }
+
+
     public List<Programacion> getprogramacionList() {
         return programacionList;
     }
@@ -80,6 +91,12 @@ public class DataTableView implements Serializable {
 
     private void createPieModels() {
         createPieModel1();
+
+        createPieModel2();
+    }
+
+    public LineChartModel getLineModel1() {
+        return lineModel1;
     }
 
     public PieChartModel getPieModel1() {
@@ -88,17 +105,72 @@ public class DataTableView implements Serializable {
 
     private void createPieModel1() {
         pieModel1 = new PieChartModel();
-
         pieModel1.set("Clientes", 199);
-
-
-
         pieModel1.setFill(false);
         pieModel1.setDiameter(150);
         pieModel1.setSeriesColors("ff0000");
         pieModel1.setShadow(false);
 
     }
+
+    private void createLineModels() {
+        lineModel1 = initLinearModel();
+        Axis xAxis = lineModel1.getAxis(AxisType.X);
+        xAxis.setMin(1);
+        xAxis.setMax(12);
+        Axis yAxis = lineModel1.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(1000000);
+
+        lineModel1.setShadow(false);
+
+    }
+
+    private LineChartModel initLinearModel() {
+        LineChartModel model = new LineChartModel();
+
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+
+        series1.set(1, 100000);
+        series1.set(2, 80000);
+        series1.set(3, 90000);
+        series1.set(4, 100000);
+        series1.set(5, 100000);
+        series1.set(6, 90000);
+        series1.set(7, 80000);
+        series1.set(8, 150000);
+        series1.set(9, 300000);
+        series1.set(10, 500000);
+        series1.set(11, 800000);
+        series1.set(12, 200000);
+
+
+        model.addSeries(series1);
+
+        return model;
+    }
+
+    public PieChartModel getPieModel2() {
+        return pieModel2;
+    }
+
+    private void createPieModel2() {
+        pieModel2 = new PieChartModel();
+
+        pieModel2.set("Brand 1", 540);
+        pieModel2.set("Brand 2", 325);
+        pieModel2.set("Brand 3", 702);
+        pieModel2.set("Brand 4", 421);
+        pieModel2.set("Brand 5", 421);
+        pieModel2.set("Brand 6", 421);
+        pieModel2.set("Brand 7", 421);
+        pieModel2.setShowDataLabels(true);
+        pieModel2.setDiameter(150);
+        pieModel2.setShadow(false);
+    }
+
+
 
 
 }
